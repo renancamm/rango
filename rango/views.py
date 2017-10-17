4b0099e2fdb022cd4634f6ea6ebddd774cf83015
Renan Cammarosano <renancamm@gmail.com>
2017-10-17 16:01:21 +0100

    adds statics, media and templates

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
+
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
+
+
+def about(request):
+    context_dict = {'name': "Beik!"}
+    return render(request, 'rango/about.html', context=context_dict)
