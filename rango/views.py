bc9e29e88215f10c96a40b3e588e1ba407251c96
Renan Cammarosano <renancamm@gmail.com>
2017-10-17 22:41:17 +0100

    adds models and admin

--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,19 @@
-
 from django.shortcuts import render
+from rango.models import Category, Page
 
 
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    return render(request, 'rango/index.html', context=context_dict)
+    # Query database and pass to context
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+
+    # Render the response and send it back!
+    return render(request, 'rango/index.html', context_dict)
 
 
 def about(request):
     context_dict = {'name': "Beik!"}
     return render(request, 'rango/about.html', context=context_dict)
+
+
+# def show_category(request, category_name_url):
